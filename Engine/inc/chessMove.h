b5cb183d73cb95690ad4b3f9619d053ee1d80cac
Fred <Fred.Clever@web.de>
2017-10-30 14:21:21 +0100

    define move improve emplacement

---
+++ Engine/inc/chessMove.h
@@ -0,0 +1,14 @@
+#pragma once
+class chessmanMove
+{
+	 
+
+public:
+
+	const int oldPosition;
+	const int newPosition;
+
+	chessmanMove(int from, int to) : oldPosition{ oldPosition; };
+	~chessmanMove();
+};
+
